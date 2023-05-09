9ccfedb034e6db767cabe2ac7eec7831d21f4bac
caofangyu <caofangyu1997@163.com>
2023-05-10 01:33:10 +0800

    完成001 特殊密码锁

--- AlgorithmBasis-GuoWei/enumerate/001.cpp
+++ AlgorithmBasis-GuoWei/enumerate/001.cpp
@@ -17,9 +17,76 @@
 
 using namespace std;
 
+void FlipBit(int& a, int index) {
+  a ^= (1 << index);
+}
+
+void SetBit(int& a, int index, char c) {
+  if (c == '1') {
+    a |= (1 << index);
+  } else {
+    a &= ~(1 << index);
+  }
+}
+
+bool GetBit(int a, int index) {
+    return (a >> index) & 1;
+}
+
+void Output(int result) {
+    if (result >= 0) {
+        cout << result;
+    } else {
+        cout << "impossible";
+    }
+
+}
+
 int main() {
   string input_str, output_str;
-  int count;
+  cin >> input_str;
+  cin >> output_str;
+  int length = input_str.length();
+  int original=0, target=0,
+      cur=0; // n < 30可以用Int保存密码锁的状态，每一位对应一个按钮的状态
+
   for (int i = 0; i < input_str.length(); ++i) {
+    SetBit(original, i, input_str[i]);
+    SetBit(target, i, output_str[i]);
   }
+
+  int min_count = 30;
+  for (int n = 0; n < 2; ++n) {
+    cur = original;
+    int count = 0;
+    if (n == 0) {
+        count++;
+        //按下第一个按钮
+        FlipBit(cur, 0);
+        if (length > 1) {
+            FlipBit(cur, 1);
+        }
+    }
+    for (int i = 1; i < length; ++i) {
+        if (GetBit(cur, i - 1) != GetBit(target, i - 1)) {
+            count++; //按下第i个按钮
+            FlipBit(cur, i - 1);
+            FlipBit(cur, i);
+            if (i + 1 < length) {
+                FlipBit(cur, i + 1);
+            }
+        }
+        if (cur == target) {
+            min_count = min(count, min_count);
+            break;
+        }
+    }
+  }
+  if (min_count < 30) {
+    cout << min_count;
+  } else {
+    cout << "impossible";
+  }
+
+  return 0;
 }
